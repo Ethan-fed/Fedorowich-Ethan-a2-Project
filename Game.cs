cf90972d44cc678f58d74c50af3cd53b57dce51c
Ethan Fedorowich <000943532@mohawkcollege.ca>
2024-10-08 23:08:47 -0400

    Update 1

--- Game.cs
+++ Game.cs
@@ -14,7 +14,12 @@ namespace Game10003
         // Place your variables here:
         float MouseX;
         float MouseY;
-
+        Color SkyBlue = new Color (10, 194, 255);
+        Color BalloonColor;
+        Color BalloonColor2;
+        Color BalloonColor3;
+        Color BalloonColor4;
+        Color BalloonColor5;
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
@@ -22,8 +27,13 @@ namespace Game10003
         {
             Window.SetTitle("Ethan's Balloon Battle");
             Window.SetSize(800, 600);
-            Window.TargetFPS = 300;
-
+            Window.TargetFPS = 120;
+            // will allow me to have the balloon change colour on each start up! (try starting it multiple times)
+            BalloonColor = Random.Color();
+            BalloonColor2 = Random.Color();
+            BalloonColor3 = Random.Color();
+            BalloonColor4 = Random.Color();
+            BalloonColor5 = Random.Color();
         }
 
         /// <summary>
@@ -36,23 +46,95 @@ namespace Game10003
             MouseY = Input.GetMouseY();
             // Clear previous image
             Window.ClearBackground(Color.White);
-           
-            Input.HideMouseCursor();
-       
-            DrawBalloon(MouseX, MouseY);
 
+            Input.HideMouseCursor();
+
+            DrawBalloon(MouseX, MouseY);
+            DrawBalloon2(MouseX + 50, MouseY + 50);
+            DrawBalloon3(MouseX + 100, MouseY );
+            DrawBalloon4(MouseX - 50, MouseY  +50);
+            DrawBalloon5(MouseX + 150, MouseY + 50);
+
+
+            //DrawCloud();
 
         }
-        // draws a balloon
+        // draws a balloon (same code multiple small variations, ie. color, +/- x/y values)
         void DrawBalloon(float x, float y)
         {
+            //draws a line that follows the circle
+            Draw.Line(x, y, x, y + 100);
             //draws a triangle that will follow the circle (put above bc i wanted it behind the circle)
-            Draw.FillColor = Color.Blue;
-            Draw.Triangle(x-20,y+50,x,y+10,x+20,y+50);
-            //draws a circle 
-            Draw.FillColor = Color.Blue;
-            Draw.Circle(x, y, 37);
+            Draw.FillColor = BalloonColor;
+            Draw.Triangle(x - 11, y + 30, x, y + 5, x + 11, y + 30);
+            //draws a circle that moves where the mouse input is
+            Draw.FillColor = BalloonColor;
+            Draw.Circle(x, y, 25);
+
+            
 
         }
+        void DrawBalloon2(float x, float y)
+        {
+            //draws a line that follows the circle
+            Draw.Line(x, y, x, y + 100);
+            //draws a triangle that will follow the circle (put above bc i wanted it behind the circle)
+            Draw.FillColor = BalloonColor2;
+            Draw.Triangle(x - 11, y + 30, x, y + 5, x + 11, y + 30);
+            //draws a circle that moves where the mouse input is
+            Draw.FillColor = BalloonColor2;
+            Draw.Circle(x, y, 25);
+
+
+
+        }
+        void DrawBalloon3(float x, float y)
+        {
+            //draws a line that follows the circle
+            Draw.Line(x, y, x, y + 100);
+            //draws a triangle that will follow the circle (put above bc i wanted it behind the circle)
+            Draw.FillColor = BalloonColor3;
+            Draw.Triangle(x - 11, y + 30, x, y + 5, x + 11, y + 30);
+            //draws a circle that moves where the mouse input is
+            Draw.FillColor = BalloonColor3;
+            Draw.Circle(x, y, 25);
+
+
+
+        }
+
+        void DrawBalloon4(float x, float y)
+        {
+            //draws a line that follows the circle
+            Draw.Line(x, y, x, y + 100);
+            //draws a triangle that will follow the circle (put above bc i wanted it behind the circle)
+            Draw.FillColor = BalloonColor4;
+            Draw.Triangle(x - 11, y + 30, x, y + 5, x + 11, y + 30);
+            //draws a circle that moves where the mouse input is
+            Draw.FillColor = BalloonColor4;
+            Draw.Circle(x, y, 25);
+
+
+
+        }
+
+        void DrawBalloon5(float x, float y)
+        {
+            //draws a line that follows the circle
+            Draw.Line(x, y, x, y + 100);
+            //draws a triangle that will follow the circle (put above bc i wanted it behind the circle)
+            Draw.FillColor = BalloonColor5;
+            Draw.Triangle(x - 11, y + 30, x, y + 5, x + 11, y + 30);
+            //draws a circle that moves where the mouse input is
+            Draw.FillColor = BalloonColor5;
+            Draw.Circle(x, y, 25);
+
+
+
+        }
+        // void DrawCloud(float x, float y);
+
+
+
     }
 }
